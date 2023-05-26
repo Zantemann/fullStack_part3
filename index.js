4aa84b998020d6b71daffd3c8d769faa3c6422b0
Santeri <santeri.ora@tuni.fi>
2023-05-26 21:41:38 +0300

    port update2

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const cors = require('cors')
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
-app.use(cors())
 
 let persons = 
 [
@@ -97,4 +97,4 @@ app.post('/api/persons', (request, response) => {
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+})
