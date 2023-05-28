7a39dbb5a605408b01ea27d8dead9f5befb4fb06
Santeri <santeri.ora@tuni.fi>
2023-05-28 17:17:18 +0300

    reset number validation

--- models/person.js
+++ models/person.js
@@ -15,11 +15,11 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-  name: String /*{
+  name: String *{
     type: String,
     minlength: 3,
     required: true
-  }*/,
+  },
   number: String,
   /*{
     type: String,
